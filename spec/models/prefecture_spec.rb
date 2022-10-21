5a9dbf5f2d5feac5ab7dbfb7087ec08aaabd0114
Jed Harwood <scaryteeth@hotmail.com>
2022-10-21 14:14:46 +0900

    established relationship between gity and prefecture and updated prefecture tests

--- spec/models/prefecture_spec.rb
+++ spec/models/prefecture_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Prefecture, type: :model do
+  it { should have_many(:cities).dependent(:destroy) }
+
   describe "validations" do
     subject { FactoryBot.build(:prefecture) }
     it { should validate_presence_of(:name) }
