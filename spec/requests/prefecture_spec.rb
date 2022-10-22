715f91245e4ad83da9c0781322ca4a96c36d4b4d
Jed Harwood <scaryteeth@hotmail.com>
2022-10-22 21:10:08 +0900

    Created prefecture controller with Show route and test

---
+++ spec/requests/prefecture_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe "Prefecture", type: :request do
+  describe "GET /show" do
+    let (:prefecture) { create(:prefecture) }
+    it "succeeds" do
+      get prefecture_path(prefecture)
+      expect(response).to be_successful
+    end
+  end
+end
