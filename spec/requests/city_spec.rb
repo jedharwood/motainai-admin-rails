1366063624a32096be29879e643cb385644939d3
Jed Harwood <scaryteeth@hotmail.com>
2022-10-24 15:21:28 +0900

    Updated city factory and added city controller basic tests

--- spec/requests/city_spec.rb
+++ spec/requests/city_spec.rb
@@ -2,6 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Cities", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "succeeds" do
+      get city_index_path
+      expect(response).to be_successful
+    end
+  end
+
+  describe "GET /show" do
+    let (:city) { create(:city) }
+    it "succeeds" do
+      get city_path(city)
+      expect(response).to be_successful
+    end
   end
 end
