50b4447636e132b3658ada13b48a66dcf813b024
Jed Harwood <scaryteeth@hotmail.com>
2022-11-04 16:03:03 +0900

    Model validation to ensure city rating in range 0..5 plus tests

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,6 @@
 class City < ApplicationRecord
   validates :name, presence: true
-  validates :rating, presence: true
+  validates :rating, presence: true, :numericality => { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
   validates :prefecture, presence: true
 
   belongs_to :prefecture
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -7,5 +7,7 @@ RSpec.describe City, type: :model do
     subject { FactoryBot.build(:city) }
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:rating) }
+    it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(0) }
+    it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5) }
   end
 end
