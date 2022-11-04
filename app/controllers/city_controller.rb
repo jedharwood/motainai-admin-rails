0613c4001a83c523acc77633caf54adec17b92fe
Jed Harwood <scaryteeth@hotmail.com>
2022-11-04 10:47:34 +0900

    Form, route and controller actions to add new city

--- app/controllers/city_controller.rb
+++ app/controllers/city_controller.rb
@@ -8,4 +8,27 @@ class CityController < ApplicationController
     def show
         @city = City.find(params[:id])
     end
+
+    def new
+        @new_city = City.new
+        @prefectures = Prefecture.all.order(:code)
+    end
+
+    def create
+        @new_city = City.new(params.require(:city).permit(:name, :prefecture_id))
+        @new_city.rating = 0
+        @prefectures = Prefecture.all.order(:code)
+
+        respond_to do |format|
+            format.html do
+                if @new_city.save
+                    flash[:success] = "City saved successfully"
+                    redirect_to city_index_path 
+                else
+                    flash.now[:error] = "Error: City could not be saved"
+                    render :new, locals: { city: @new_city }, status: :unprocessable_entity
+                end
+            end
+        end
+    end
 end
