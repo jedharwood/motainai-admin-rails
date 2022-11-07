1ba57b0140090215ccc3f013f0d977938924e82e
Jed Harwood <scaryteeth@hotmail.com>
2022-11-07 15:12:30 +0900

    City controller new method refactor

--- app/controllers/city_controller.rb
+++ app/controllers/city_controller.rb
@@ -1,62 +1,62 @@
 # frozen_string_literal: true
 
 class CityController < ApplicationController
-    def index
-        @cities = City.all.order(:name)
-    end
+  def index
+    @cities = City.all.order(:name)
+  end
 
-    def show
-        @city = City.find(params[:id])
-    end
+  def show
+    @city = City.find(params[:id])
+  end
 
-    def new
-        @prefectures = Prefecture.all.order(:code)
-        @new_city = City.new
-        if params[:format].present?
-            @new_city.prefecture_id = params[:format]
+  def new
+    @prefectures = Prefecture.all.order(:code)
+    @new_city = City.new
+    return unless params[:format].present?
+
+    @new_city.prefecture_id = params[:format]
+  end
+
+  def create
+    @prefectures = Prefecture.all.order(:code)
+    @new_city = City.new(params.require(:city).permit(:name, :prefecture_id))
+    @new_city.rating = 0
+
+    respond_to do |format|
+      format.html do
+        if @new_city.save
+          flash[:success] = 'City saved successfully'
+          redirect_to city_index_path
+        else
+          flash.now[:error] = 'Error: City could not be saved'
+          render :new, locals: { city: @new_city }, status: :unprocessable_entity
         end
+      end
     end
+  end
 
-    def create
-        @prefectures = Prefecture.all.order(:code)
-        @new_city = City.new(params.require(:city).permit(:name, :prefecture_id))
-        @new_city.rating = 0
+  def edit
+    @prefectures = Prefecture.all.order(:code)
+    @city = City.find(params[:id])
+    respond_to do |format|
+      format.html { render :edit, locals: { city: @city } }
+    end
+  end
 
-        respond_to do |format|
-            format.html do
-                if @new_city.save
-                    flash[:success] = "City saved successfully"
-                    redirect_to city_index_path 
-                else
-                    flash.now[:error] = "Error: City could not be saved"
-                    render :new, locals: { city: @new_city }, status: :unprocessable_entity
-                end
-            end
+  def update
+    @prefectures = Prefecture.all.order(:code)
+    @city = City.find(params[:id])
+
+    respond_to do |format|
+      format.html do
+        if @city.update(params.require(:city).permit(:name, :prefecture_id, :rating))
+          flash[:success] = 'City updated successfully'
+          redirect_to city_path(@city)
+        else
+          flash.now[:error] = 'Error: City could not be updated'
+          render :edit, locals: { city: @city }, status: :unprocessable_entity
         end
+      end
     end
-
-    def edit
-        @prefectures = Prefecture.all.order(:code)
-        @city = City.find(params[:id])
-        respond_to do |format|
-            format.html { render :edit, locals: { city: @city } }
-        end
-    end
-
-    def update
-        @prefectures = Prefecture.all.order(:code)
-        @city = City.find(params[:id])
-
-        respond_to do |format|
-            format.html do
-                if @city.update(params.require(:city).permit(:name, :prefecture_id, :rating))
-                    flash[:success] = 'City updated successfully'
-                    redirect_to city_path(@city)
-                else
-                    flash.now[:error] = 'Error: City could not be updated'
-                    render :edit, locals: { city: @city }, status: :unprocessable_entity
-                end
-            end
-        end
-    end
+  end
 end
