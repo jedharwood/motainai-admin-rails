200e6ac99a6bd615dbd65a00850234c96572993b
Jed Harwood <scaryteeth@hotmail.com>
2022-11-08 14:37:43 +0900

    Refactored user-name and user-initials methods

--- app/helpers/user_profile_helper.rb
+++ app/helpers/user_profile_helper.rb
@@ -5,20 +5,17 @@ module UserProfileHelper
     user_signed_in? && current_user&.profile&.approved
   end
 
-  def user_profile_initials
-    @profile = current_user&.profile
-    user_signed_in? && user_has_full_name ? "#{@profile&.first_name&.[](0)}#{@profile&.last_name&.[](0)}" : 'MA'
+  def user_profile_initials(signed_in, profile)
+    signed_in && user_has_full_name(profile) ? "#{profile&.first_name&.[](0)}#{profile&.last_name&.[](0)}" : 'MA'
   end
 
-  def user_profile_full_name
-    @profile = current_user&.profile
-    user_signed_in? && user_has_full_name ? "#{@profile&.first_name} #{@profile&.last_name}" : ''
+  def user_profile_full_name(signed_in, profile)
+    signed_in && user_has_full_name(profile) ? "#{profile&.first_name} #{profile&.last_name}" : ''
   end
 
   private
 
-  def user_has_full_name
-    @profile = current_user&.profile
-    @profile&.first_name && @profile&.last_name
+  def user_has_full_name(profile)
+    profile&.first_name && profile&.last_name
   end
 end
