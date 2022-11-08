fa6d32241a18798a9ee6c1c984974cd9c9a0c343
Jed Harwood <scaryteeth@hotmail.com>
2022-11-08 15:16:30 +0900

    Refactored user_profile_spec

--- spec/helpers/user_profile_spec.rb
+++ spec/helpers/user_profile_spec.rb
@@ -2,13 +2,13 @@
 
 require 'rails_helper'
 
-RSpec.describe UserProfileHelper, type: :helper do
-  signed_in = true
-  not_signed_in = false
-  profile_with_full_name = Profile.create(first_name: 'Cliff', last_name: 'Burton', approved: true)
-  profile_with_first_name = Profile.create(first_name: 'Cliff', approved: false)
-  profile_with_last_name = Profile.create(last_name: 'Burton')
+signed_in = true
+not_signed_in = false
+profile_with_full_name = Profile.create(first_name: 'Cliff', last_name: 'Burton', approved: true)
+profile_with_first_name = Profile.create(first_name: 'Cliff', approved: false)
+profile_with_last_name = Profile.create(last_name: 'Burton')
 
+RSpec.describe UserProfileHelper, type: :helper do
   describe 'approved_user?' do
     it 'returns false if !user_signed_in' do
       result = approved_user?(not_signed_in, profile_with_full_name)
@@ -25,7 +25,9 @@ RSpec.describe UserProfileHelper, type: :helper do
       expect(result).to be true
     end
   end
+end
 
+RSpec.describe UserProfileHelper, type: :helper do
   describe 'user_profile_initials' do
     it 'returns MA if !user_signed_in' do
       result = user_profile_initials(not_signed_in, profile_with_full_name)
@@ -33,14 +35,12 @@ RSpec.describe UserProfileHelper, type: :helper do
     end
 
     it 'returns MA if user_signed_in and profile has only first_name' do
-      profile = Profile.create(first_name: 'Cliff')
-      result = user_profile_initials(signed_in, profile)
+      result = user_profile_initials(signed_in, profile_with_first_name)
       expect(result).to eq 'MA'
     end
 
     it 'returns MA if user_signed_in and profile has only last_name' do
-      profile = Profile.create(last_name: 'Burton')
-      result = user_profile_initials(signed_in, profile)
+      result = user_profile_initials(signed_in, profile_with_last_name)
       expect(result).to eq 'MA'
     end
 
@@ -49,7 +49,9 @@ RSpec.describe UserProfileHelper, type: :helper do
       expect(result).to eq 'CB'
     end
   end
+end
 
+RSpec.describe UserProfileHelper, type: :helper do
   describe 'user_profile_full_name' do
     it 'returns empty string if !user_signed_in' do
       result = user_profile_full_name(not_signed_in, profile_with_full_name)
