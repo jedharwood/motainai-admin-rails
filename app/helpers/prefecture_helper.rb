7e5e5f7741285b59b19abb3c99afecd77b752205
Jed Harwood <scaryteeth@hotmail.com>
2022-11-14 12:39:16 +0900

    Added get number of rules by city id method to helper and implemented it in view

--- app/helpers/prefecture_helper.rb
+++ app/helpers/prefecture_helper.rb
@@ -4,4 +4,8 @@ module PrefectureHelper
   def get_number_of_cities_by_prefecture_id(params)
     City.where(prefecture_id: params[:id]).count
   end
+
+  def get_number_of_rules_by_city_id(params)
+     WasteType.where(city_identifier: params[:id]).count 
+  end
 end
