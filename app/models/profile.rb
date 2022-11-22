a07e7236c2f6a46f9fc6fb72d24fce37edddfe19
Jed Harwood <scaryteeth@hotmail.com>
2022-11-22 15:51:47 +0900

    Added validation and tests to profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class Profile < ApplicationRecord
+  validates :first_name, presence: true, on: :update
+  validates :last_name, presence: true, on: :update
+
   belongs_to :user
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -4,4 +4,12 @@ require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
   it { belong_to :user }
+
+  describe 'validations' do
+    subject { FactoryBot.build(:profile) }
+    it { should validate_presence_of(:first_name).on(:update) }
+    it { should validate_presence_of(:last_name).on(:update) }
+    it { should_not validate_presence_of(:first_name).on(:create) }
+    it { should_not validate_presence_of(:last_name).on(:create) }
+  end
 end
