2577240b9232ccd0ab10b04f3b222cd528eaf4e2
Jed Harwood <scaryteeth@hotmail.com>
2022-11-26 12:17:17 +0900

    Added mailer call to registrations controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
 
     def after_sign_up_path_for(resource)
         @profile = Profile.find_by(user_id: resource.id)
+        ProfileMailer.with(user: @profile.user).update_profile.deliver_later
         edit_profile_path(@profile)
     end
 end
