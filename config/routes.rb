df695d6bccd75917acd46e0e47ac706dd85bb40e
Jed Harwood <scaryteeth@hotmail.com>
2022-11-26 12:20:54 +0900

    rubocop single quoted

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   # devise_for :users
-  devise_for :users, controllers: { registrations: "registrations" }
+  devise_for :users, controllers: { registrations: 'registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
