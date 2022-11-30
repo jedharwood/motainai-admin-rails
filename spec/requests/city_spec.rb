e8358dabf3b41eb5efe1c06b9c15d448e5600c96
Jed Harwood <scaryteeth@hotmail.com>
2022-11-30 13:20:25 +0900

    City/Index

--- spec/requests/city_spec.rb
+++ spec/requests/city_spec.rb
@@ -4,15 +4,30 @@ require 'rails_helper'
 
 RSpec.describe 'Cities', type: :request do
   describe 'GET /index' do
+    let(:city_list) { create_list(:city, 3) } 
+      
     it 'succeeds' do
       get city_index_path
       expect(response).to be_successful
+      expect(response.status).to eq(200)
       expect(response).to render_template(:index)
+    end
+
+    it 'renders index template' do
+      get city_index_path
+      expect(response).to render_template(:index)
+    end
+
+    it "assigns @cities" do
+      get city_index_path
+      expected = city_list.sort_by { |city| city.name } 
+      expect(assigns(:cities)).to eq(expected)
     end
   end
 
   describe 'GET /show' do
     let(:city) { create(:city) }
+    
     it 'succeeds' do
       get city_path(city)
       expect(response).to be_successful
